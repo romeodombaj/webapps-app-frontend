513aa6bc26bb1966c320e4cc91e70789e49b658b
romeodombaj <romeodombaj@gmail.com>
2023-03-29 22:51:52 +0200

    Initial commit

---
+++ src/components/navigation/NavBar.js
@@ -0,0 +1,7 @@
+import styles from "./NavBar.module.css";
+
+const NavBar = () => {
+  return <h2>Navigation</h2>;
+};
+
+export default NavBar;
---
+++ src/components/recipes/RecipeGallery.js
@@ -0,0 +1,7 @@
+import styles from "./RecipesGallery.module.css";
+
+const RecipesGallery = () => {
+  return <h2>Gallery</h2>;
+};
+
+export default RecipesGallery;
