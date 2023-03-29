5c669811caff902a3c1ef944c4c3b21b18ab9ab8
romeodombaj <romeodombaj@gmail.com>
2023-03-29 23:04:12 +0200

    NavBar positioning

--- src/components/navigation/NavBar.js
+++ src/components/navigation/NavBar.js
@@ -1,7 +1,13 @@
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
-  return <h2>Navigation</h2>;
+  return (
+    <div className={styles.wrapper}>
+      <h1>1</h1>
+      <h1>2</h1>
+      <h1>3</h1>
+    </div>
+  );
 };
 
 export default NavBar;
