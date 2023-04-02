82729c1eab563d76ccd0c49f7dcf1bbd8d15dcb5
romeodombaj <romeodombaj@gmail.com>
2023-04-02 21:01:48 +0200

    profile section added

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,9 +1,23 @@
 import styles from "./Profile.module.css";
 
+const dummy_profile = {
+  id: 0,
+  name: "Bob",
+  surname: "Doe",
+  username: "bDoe23",
+  age: "32",
+};
+
 const Profile = () => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <h1>PROFILE</h1>
+      <div className={styles[`personal-info-wrapper`]}>
+        <div className={styles.text}>Name: {dummy_profile.name}</div>
+        <div className={styles.text}>Surname: {dummy_profile.surname}</div>
+        <div className={styles.text}>Age: {dummy_profile.age}</div>
+        <div className={styles.text}>Username: {dummy_profile.username}</div>
+      </div>
     </div>
   );
 };
